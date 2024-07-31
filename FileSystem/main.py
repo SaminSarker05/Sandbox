55fb590ff714158ee653342fe0aee5c2355297cb
Samin Sarker <saminsarker05@gmail.com>
2024-07-31 00:00:04 -0400

    new learning

---
+++ FileSystem/main.py
@@ -0,0 +1,35 @@
+# 00. FUNCTIONS
+
+file = open('file_name.txt', "r")
+for line in file:
+  print(line)
+file.close()  # close file to release resources
+
+f = open("new_file", "w")
+f.write("Hello world")
+f.close() 
+
+
+# 01. WITH METHOD (BEST PRACTICE) - automatically closes file using a context manager
+# often used with file systems to ensure file is closed after use
+# context manager runs a enter and exit protocal betore/after block of code
+
+# open function returns a file object and AS keyword assigns it a variable
+# with ensures proper closing even if exception
+with open('file_name.txt', "r") as f:
+  for line in f:
+    print(line)
+
+
+# 02. try except block
+try:
+  with open("does_not_exist", "r") as f:
+    pass
+except Exception as e:
+  print(e)
+finally:
+  print("test done")
+
+
+"""
+"""
