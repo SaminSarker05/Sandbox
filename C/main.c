e847612b4c2cdc744531049667a113a160f376cd
Samin Sarker <saminsarker05@gmail.com>
2024-08-18 00:14:24 -0400

    more learning

---
+++ C/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  /*
+    \n newline
+    \t tab
+  */
+  printf("HELLO WORLD...\n");
+
+  int age = 21;
+  float gpa = 2.05;
+  char grade = 'C';
+  char name[] = "string"; // string is an array of characters
+}
+
+// https://www.youtube.com/watch?v=87SH2Cn0s9A 26min
