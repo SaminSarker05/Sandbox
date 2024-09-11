cc1e99695a425add5dc4ab0a264716ddbcb0abba
Samin Sarker <saminsarker05@gmail.com>
2024-09-11 16:30:55 -0400

    conditionals with locks

--- Concurrency/main.cpp
+++ Concurrency/main.cpp
@@ -1,31 +1,42 @@
-#include <thread>
 #include <iostream>
-#include <vector>
-#include <mutex>
-#include <atomic>
+#include <thread>
+#include <chrono> // use for sleeping
+#include <condition_variable>
 
 std::mutex gLock;
-
-// constant variable that is allocated for program lifetime and value carries over
-static std::atomic<int> shared_value = 0;
-void increment_shared_value() {
-  // lock access to shared resource while thread is running
-  // gLock.lock();
-  ++shared_value;
-  // gLock.unlock();
-  // error with threads as threads could be reading and writing at same time
-  // fix by using std::mutex --> gives threads mutually exlusive access to a shared resource
-}
+std::condition_variable gConditionVariable;
 
 int main() {
-  auto lambda = [](int x) {std::cout << "arg: " << x << std::endl;};
+  int result = 0;
+  bool notified = false;
 
-  std::vector<std::thread> threads;
-  for (int i = 0; i < 1000; i++) {
-    threads.push_back(std::thread(increment_shared_value));
-  }
-  for (int i = 0; i < threads.size(); i++) {
-    threads[i].join();
-  }
-  std::cout << "threads have finished executing... shared value: " << shared_value << std::endl;
+  // reporting thread
+  // must wait on worker thread
+  std::thread reporter([&]{
+    std::unique_lock<std::mutex> lock(gLock);
+    // even if reporter thread executes first, it is blocked if not notified
+    if (!notified) {
+      // thread will sleep until notified/unblocked
+      gConditionVariable.wait(lock); // unlocks lock when notified
+    }
+    std::cout << "reporting result: " << result << std::endl;
+  });
+
+  // working thread
+  std::thread worker([&]{
+    // more powerful then normal locks
+    std::unique_lock<std::mutex> lock(gLock);
+    notified = true;
+    result++;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::cout << "work done" << std::endl;
+
+    gConditionVariable.notify_one();  // wake up any sleeping threads
+  });
+
+  // join threads so main thread does not finish before worker and reporter
+  reporter.join();
+  worker.join();
+
+  return 0;
 }
